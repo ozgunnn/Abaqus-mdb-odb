4e591aabb2fdca19fe14e0c1f534300e2e55465f
ozgunnn <ozgunergun@gmail.com>
2022-08-19 12:12:40 +0200

    Initialize

---
+++ mdbsetpicker.py
@@ -0,0 +1,25 @@
+partName = 'concrete'
+modelName = 'A12'
+
+partToInstance = {
+    "concrete": 'Concrete Instance',
+    'Beam': 'Profile Instance',
+}
+
+set = mdb.models[modelName].parts[partName].Set(
+    elements=mdb.models[modelName].parts[partName].elements.getByBoundingBox(zMin=1550, zMax=1590), name='z1550')
+
+regionDef = mdb.models['A12'].rootAssembly.allInstances[partToInstance[partName]].sets['z1550']
+
+mdb.models[modelName].HistoryOutputRequest(name='H-Output-2',
+                                           createStepName='Step-1', variables=('LE11', 'LE22', 'LE33', 'LE12', 'LE13',
+                                                                               'LE23', 'LEP'), region=regionDef, sectionPoints=DEFAULT)
+
+elementNames = []
+
+for i in set.elements:
+    elementNames.append(i.label)
+
+print(elementNames)
+
+# Create history output request for these nodes to reach them in odb
---
+++ odbmanage.py
@@ -0,0 +1,30 @@
+from abaqusConstants import *
+from abaqus import *
+import csv
+from odbAccess import *
+from caeModules import *
+from textRepr import *
+##open odb
+odb = openOdb(path='Job_section.odb')
+
+##in this line, only one element of chosen region is picked
+elementNodes = odb.steps['Step-1'].historyRegions.values()[1].point.element.connectivity
+elementLabel = odb.steps['Step-1'].historyRegions.values()[1].point.element.label
+##all nodes in the part of interest are chosen
+allNodes = odb.rootAssembly.instances['PART-1-1'].nodes
+
+xSum = 0
+ySum = 0
+zSum = 0
+
+##query the nodes with labels of vertices of element of interest and average them to find element centroid
+for i in elementNodes:
+    node = [node for node in allNodes if node.label == i]
+    xSum += node[0].coordinates[0]
+    ySum += node[0].coordinates[0]
+    zSum += node[0].coordinates[0]
+    midpoint = [xSum/8,ySum/8,zSum/8]
+
+print("centroid of element: ",elementLabel,"is ",midpoint)
+
+##why need mdb?
