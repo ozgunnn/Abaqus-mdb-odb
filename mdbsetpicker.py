a8f481433e9361c1b243ef5d200f843ac09d9df7
ozgunnn <ozgunergun@gmail.com>
2022-08-22 21:50:47 +0200

    text mining

--- mdbsetpicker.py
+++ mdbsetpicker.py
@@ -6,15 +6,26 @@ partToInstance = {
     'Beam': 'Profile Instance',
 }
 
+a = mdb.models['A12'].rootAssembly
+
 set = mdb.models[modelName].parts[partName].Set(
     elements=mdb.models[modelName].parts[partName].elements.getByBoundingBox(zMin=1550, zMax=1590), name='z1550')
 
-regionDef = mdb.models['A12'].rootAssembly.allInstances[partToInstance[partName]].sets['z1550']
+regionDef = a.allInstances[partToInstance[partName]].sets['z1550']
 
 mdb.models[modelName].HistoryOutputRequest(name='H-Output-2',
                                            createStepName='Step-1', variables=('LE11', 'LE22', 'LE33', 'LE12', 'LE13',
                                                                                'LE23', 'LEP'), region=regionDef, sectionPoints=DEFAULT)
-sa
+
+r1 = a.referencePoints
+refPoints1 = (r1[r1.keys()[0]], )
+a.Set(referencePoints=refPoints1, name='Set-RP')
+
+regionDef = mdb.models['A12'].rootAssembly.sets['Set-RP']
+mdb.models['A12'].HistoryOutputRequest(name='H-Output-3',
+                                       createStepName='Step-1', variables=('RF3', ), region=regionDef,
+                                       sectionPoints=DEFAULT)
+
 elementNames = []
 
 for i in set.elements:
--- odbmanage.py
+++ odbmanage.py
@@ -4,27 +4,48 @@ import csv
 from odbAccess import *
 from caeModules import *
 from textRepr import *
-##open odb
-odb = openOdb(path='Job_section.odb')
+# open odb
+odb = openOdb(path='Job_A12.odb')
 
-##in this line, only one element of chosen region is picked
-elementNodes = odb.steps['Step-1'].historyRegions.values()[1].point.element.connectivity
-elementLabel = odb.steps['Step-1'].historyRegions.values()[1].point.element.label
-##all nodes in the part of interest are chosen
-allNodes = odb.rootAssembly.instances['PART-1-1'].nodes
+# initialize empty list
+elementNames = []
 
-xSum = 0
-ySum = 0
-zSum = 0
+# populate the list based on concrete instance history output regions. This loop extracts the element number from dictionary keys
+for text in odb.steps['Step-1'].historyRegions.keys():
+    left = 'Concrete Instance.'
+    right = ' Int'
+    try:
+        found = text[text.index(left)+len(left):text.index(right)]
+        elementNames.append(int(found))
+    except:
+        pass
 
-##query the nodes with labels of vertices of element of interest and average them to find element centroid
-for i in elementNodes:
-    node = [node for node in allNodes if node.label == i]
-    xSum += node[0].coordinates[0]
-    ySum += node[0].coordinates[0]
-    zSum += node[0].coordinates[0]
-    midpoint = [xSum/8,ySum/8,zSum/8]
+print(elementNames)
 
-print("centroid of element: ",elementLabel,"is ",midpoint)
+# all nodes in the part of interest are chosen
+allNodes = odb.rootAssembly.instances['Concrete Instance'].nodes[:]
 
-##why need mdb?
+# connectivities of each element obtained with this loop
+for i in elementNames:
+    search = '.' + str(i) + ' '
+    print('search', search)
+    key = [key for key in odb.steps['Step-1'].historyRegions.keys()
+           if search in key]
+    print('key', key[0])
+    elementNodes = odb.steps['Step-1'].historyRegions[key[0]
+                                                      ].point.element.connectivity
+    print(elementNodes)
+    xSum = 0
+    ySum = 0
+    zSum = 0
+# query the nodes with labels of vertices of element of interest and average them to find element centroid
+    for i in elementNodes:
+        node = [node for node in allNodes if node.label == i]
+        xSum += node[0].coordinates[0]
+        ySum += node[0].coordinates[0]
+        zSum += node[0].coordinates[0]
+        midpoint = [xSum/8, ySum/8, zSum/8]
+
+    print("centroid of element: ", i, "is ", midpoint)
+
+# why need mdb?
