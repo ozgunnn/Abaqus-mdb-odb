26829eb1698dd3330db20924197b60cc640b3fe3
Ozgun <ozgunergun@gmail.com>
2022-08-25 09:59:25 +0200

    Code Cleared

--- mdbsetpicker.py
+++ mdbsetpicker.py
@@ -1,40 +1,39 @@
 partName = 'concrete'
 modelName = 'A12'
+halfLength = 3000
+noCuts = 4
 
 partToInstance = {
     "concrete": 'Concrete Instance',
     'Beam': 'Profile Instance',
 }
 
-a = mdb.models['A12'].rootAssembly
-
+rootA = mdb.models[modelName].rootAssembly
+partC = mdb.models[modelName].parts[partName]
 cutHeights = []
-halfLength = 3000
-noCuts = 4
+
 for i in range(1,noCuts+1):
     cutHeights.append(halfLength/noCuts*i)
 
 for i in cutHeights:
-    mdb.models[modelName].parts[partName].Set(elements=mdb.models[modelName].parts[partName].elements.getByBoundingBox(zMin=i-50, zMax=i), name='z'+str(i))
-    regionDef = a.allInstances[partToInstance[partName]].sets['z'+str(i)]
+    partC.Set(elements=partC.elements.getByBoundingBox(zMin=i-50, zMax=i), name='z'+str(i))
+    regionDef = rootA.allInstances[partToInstance[partName]].sets['z'+str(i)]
     mdb.models[modelName].HistoryOutputRequest(name='H-Output-'+'z'+str(i),numIntervals=180,createStepName='Step-1', variables=('LE33',), region=regionDef, sectionPoints=DEFAULT)
 
-r1 = a.referencePoints
+r1 = rootA.referencePoints
 refPoints1 = (r1[r1.keys()[0]], )
-a.Set(referencePoints=refPoints1, name='Set-RP')
+rootA.Set(referencePoints=refPoints1, name='Set-RP')
 
-regionDef = mdb.models['A12'].rootAssembly.sets['Set-RP']
-mdb.models['A12'].HistoryOutputRequest(name='H-Output-3',
+regionDef = rootA.sets['Set-RP']
+mdb.models[modelName].HistoryOutputRequest(name='H-Output-3',
                                        createStepName='Step-1' ,numIntervals=180,variables=('RF3', ), region=regionDef,
                                        sectionPoints=DEFAULT)
 
 for h in cutHeights:
-    for i in mdb.models['A12'].parts['concrete'].sets['z'+str(h)].elements:
+    for i in partC.sets['z'+str(h)].elements:
         elo=i.label
-        mdb.models[modelName].parts[partName].SetFromElementLabels(elementLabels=(elo,), name='element'+str(elo))
-        regionDef = mdb.models['A12'].rootAssembly.allInstances[partToInstance[partName]].sets['element'+str(elo)]
-        #regionDef = mdb.models['A12'].parts['concrete'].sets['element'+str(elo)]
+        partC.SetFromElementLabels(elementLabels=(elo,), name='element'+str(elo))
+        regionDef = rootA.allInstances[partToInstance[partName]].sets['element'+str(elo)]
         mdb.models[modelName].HistoryOutputRequest(name='H-Output-'+str(elo),createStepName='Step-1',numIntervals=180, variables=('COORDCOM1','COORDCOM2','COORDCOM3',), region=regionDef, sectionPoints=DEFAULT)
-
 
 # Create history output request for these nodes to reach them in odb
