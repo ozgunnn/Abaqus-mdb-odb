788f6c1a8c1580479b2766314e5b0b4c45ce1854
ozgunnn <ozgunergun@gmail.com>
2022-10-22 00:23:57 +0200

    added midspan deflection output

--- mdbsetpicker.py
+++ mdbsetpicker.py
@@ -2,6 +2,7 @@ partName = 'concrete'
 modelName = '1_geoimp'
 halfLength = 1750.0
 noCuts = 5
+axis = "Weak"
 
 partToInstance = {
     "concrete": 'Concrete Instance',
@@ -23,14 +24,34 @@ for i in cutHeights:
     mdb.models[modelName].HistoryOutputRequest(name='H-Output-'+'z'+str(
         int(i)), numIntervals=180, createStepName='Step-1', variables=('LE33',), region=regionDef, sectionPoints=DEFAULT)
 
+# create set of shell element at midspan
+partBeam = mdb.models[modelName].parts['Beam']
+partBeam.Set(elements=partBeam.elements.getByBoundingBox(
+    xMin=-5, xMax=5, yMin=-45, yMax=45, zMin=1750-50, zMax=1750+10), name='mid_node')
+partBeam.SetFromElementLabels(
+    elementLabels=partBeam.sets['mid_node'].elements[0].label, name='mid_node')
+
 r1 = rootA.referencePoints
 refPoints1 = (r1[r1.keys()[0]], )
 rootA.Set(referencePoints=refPoints1, name='Set-RP')
 
+# create history output of force
 regionDef = rootA.sets['Set-RP']
 mdb.models[modelName].HistoryOutputRequest(name='H-Output-3',
                                            createStepName='Step-1', numIntervals=180, variables=('RF3', ), region=regionDef,
                                            sectionPoints=DEFAULT)
+
+# create history output of deflection
+myRegion = rootA.allInstances[partToInstance['Beam']
+                              ].sets['mid_node']
+if axis == 'Weak':
+    mdb.models[modelName].HistoryOutputRequest(name='H-Output-def',
+                                               createStepName='Step-1', numIntervals=180, variables=('U1', ), region=myRegion,
+                                               sectionPoints=DEFAULT)
+if axis == 'Strong':
+    mdb.models[modelName].HistoryOutputRequest(name='H-Output-def',
+                                               createStepName='Step-1', numIntervals=180, variables=('U2', ), region=regionDef,
+                                               sectionPoints=DEFAULT)
 
 for h in cutHeights:
     for i in partC.sets['z'+str(int(h))].elements:
